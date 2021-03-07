73020144a6521c3444d842a62518a43dccf9f8d8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-07 01:55:22 +0100

    pythonPackages.nbconvert: move override to right place

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4366,7 +4366,9 @@ in {
 
   nbconflux = callPackage ../development/python-modules/nbconflux { };
 
-  nbconvert = callPackage ../development/python-modules/nbconvert { };
+  nbconvert = callPackage ../development/python-modules/nbconvert {
+    nbclient = self.nbclient.override { doCheck = false; };
+  };
 
   nbdime = callPackage ../development/python-modules/nbdime { };
 
