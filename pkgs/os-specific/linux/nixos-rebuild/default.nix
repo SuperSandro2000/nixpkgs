c9066ed95c0e71c1088a26d029c887548ecb2e5b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-09-04 13:04:56 +0200

    nixos-rebuild: pipe through nom

--- pkgs/os-specific/linux/nixos-rebuild/default.nix
+++ pkgs/os-specific/linux/nixos-rebuild/default.nix
@@ -5,6 +5,7 @@
 , gnugrep
 , nix
 , lib
+, nix-output-monitor
 , nixosTests
 }:
 let
@@ -19,7 +20,7 @@ substituteAll {
   nix_x86_64_linux = fallback.x86_64-linux;
   nix_i686_linux = fallback.i686-linux;
   nix_aarch64_linux = fallback.aarch64-linux;
-  path = lib.makeBinPath [ coreutils gnused gnugrep ];
+  path = lib.makeBinPath [ coreutils gnused gnugrep nix-output-monitor ];
 
   # run some a simple installer tests to make sure nixos-rebuild still works for them
   passthru.tests = {
