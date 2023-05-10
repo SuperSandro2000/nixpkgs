1d4f58d31a7dac82e035f5e0c9f79bc53073169d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-05-11 01:00:01 +0200

    nixos/no-x-libs: gst_all_1.gst-plugins-base: disable wayland to disable GL
    GL was already participially disabled because X11 is disabled and lead to
    the following error when building gst-plguins-good:
    
    ```
    Did not find CMake 'cmake'
    Found CMake: NO
    Run-time dependency gstreamer-gl-prototypes-1.0 found: NO (tried pkgconfig and cmake)
    Looking for a fallback subproject for the dependency gstreamer-gl-prototypes-1.0
    
    meson.build:328:2: ERROR: Neither a subproject directory nor a gst-plugins-base.wrap file was found.
    ```

--- nixos/modules/config/no-x-libs.nix
+++ nixos/modules/config/no-x-libs.nix
@@ -38,7 +38,7 @@ with lib;
       gpsd = super.gpsd.override { guiSupport = false; };
       graphviz = super.graphviz-nox;
       gst_all_1 = super.gst_all_1 // {
-        gst-plugins-base = super.gst_all_1.gst-plugins-base.override { enableX11 = false; };
+        gst-plugins-base = super.gst_all_1.gst-plugins-base.override { enableWayland = false; enableX11 = false; };
       };
       imagemagick = super.imagemagick.override { libX11Support = false; libXtSupport = false; };
       imagemagickBig = super.imagemagickBig.override { libX11Support = false; libXtSupport = false; };
