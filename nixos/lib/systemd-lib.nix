bd32d2b98452efa485a44c5ccbb306cd06920980
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-06-07 00:05:18 +0200

    nixos/systemd-lib: discard string context for X-Restart-Triggers
    The files only track if the service unit should be restarted, the actual
    contents are irrelevant.

--- nixos/lib/systemd-lib.nix
+++ nixos/lib/systemd-lib.nix
@@ -618,7 +618,13 @@ rec {
           X-Restart-Triggers = "${pkgs.writeText "X-Restart-Triggers-${name}" (
             pipe config.restartTriggers [
               flatten
-              (map (x: if isPath x then "${x}" else x))
+              (map (
+                x:
+                if isPath x then
+                  "${builtins.unsafeDiscardStringContext x}"
+                else
+                  builtins.unsafeDiscardStringContext x
+              ))
               toString
             ]
           )}";
@@ -627,7 +633,13 @@ rec {
           X-Reload-Triggers = "${pkgs.writeText "X-Reload-Triggers-${name}" (
             pipe config.reloadTriggers [
               flatten
-              (map (x: if isPath x then "${x}" else x))
+              (map (
+                x:
+                if isPath x then
+                  "${builtins.unsafeDiscardStringContext x}"
+                else
+                  builtins.unsafeDiscardStringContext x
+              ))
               toString
             ]
           )}";
