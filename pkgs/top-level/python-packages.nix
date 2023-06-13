4c98ac8bde0fa972bf39d6134e466990379e5dfc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-06-13 16:11:10 +0200

    python310.pkgs.recline: init at 2023.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10531,6 +10531,8 @@ self: super: with self; {
 
   recipe-scrapers = callPackage ../development/python-modules/recipe-scrapers { };
 
+  recline = callPackage ../development/python-modules/recline { };
+
   recoll = toPythonModule (pkgs.recoll.override {
     python3Packages = self;
   });
