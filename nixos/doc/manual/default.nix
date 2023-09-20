88e0faebd96d295d114c7f38fcedb288e95ade5e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-09-21 00:06:52 +0200

    nixos/man: build locally

--- nixos/doc/manual/default.nix
+++ nixos/doc/manual/default.nix
@@ -230,7 +230,7 @@ rec {
 
   # Generate the `man configuration.nix` package
   nixos-configuration-reference-manpage =
-    runCommand "nixos-configuration-reference-manpage"
+    pkgs.runCommandLocal "nixos-configuration-reference-manpage"
       {
         nativeBuildInputs = [
           buildPackages.installShellFiles
