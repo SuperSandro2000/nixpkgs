c76efc783066062b5095c41843ab8774c1063ad5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-10-13 11:21:19 +0200

    nixos/avahi-daemon: read daemon config from /etc/avahi/avahi-daemon.conf

--- nixos/modules/services/networking/avahi-daemon.nix
+++ nixos/modules/services/networking/avahi-daemon.nix
@@ -310,14 +310,15 @@ in
 
     environment.systemPackages = [ cfg.package ];
 
-    environment.etc = (
-      lib.mapAttrs' (
-        n: v:
-        lib.nameValuePair "avahi/services/${n}.service" {
-          ${if lib.types.path.check v then "source" else "text"} = v;
-        }
-      ) cfg.extraServiceFiles
-    );
+    environment.etc = {
+      "avahi/avahi-daemon.conf".source = avahiDaemonConf;
+    }
+    // (lib.mapAttrs' (
+      n: v:
+      lib.nameValuePair "avahi/services/${n}.service" {
+        ${if lib.types.path.check v then "source" else "text"} = v;
+      }
+    ) cfg.extraServiceFiles);
 
     systemd.sockets.avahi-daemon = {
       description = "Avahi mDNS/DNS-SD Stack Activation Socket";
@@ -356,7 +357,7 @@ in
         NotifyAccess = "main";
         BusName = "org.freedesktop.Avahi";
         Type = "dbus";
-        ExecStart = "${cfg.package}/sbin/avahi-daemon --syslog -f ${avahiDaemonConf} ${lib.optionalString cfg.debug "--debug"}";
+        ExecStart = "${cfg.package}/sbin/avahi-daemon --syslog -f /etc/avahi/avahi-daemon.conf ${lib.optionalString cfg.debug "--debug"}";
         ConfigurationDirectory = "avahi/services";
 
         # Hardening
