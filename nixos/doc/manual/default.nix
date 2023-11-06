b1196831540e21973047b1e24cb79783031ed790
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-11-06 01:15:09 +0100

    nixos: reduce useless rebuilds when changing anything in modules by using ca

--- nixos/doc/manual/default.nix
+++ nixos/doc/manual/default.nix
@@ -289,6 +289,7 @@ rec {
           buildPackages.nixos-render-docs
         ];
         allowedReferences = [ "out" ];
+        __contentAddressed = true;
       }
       ''
         # Generate manpages.
