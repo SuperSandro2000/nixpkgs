99325ea8e20c52e6d377dbc92faba2ef92e4229b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-11-27 13:42:00 +0100

    gitea: 1.21.0 -> 1.21.1

--- pkgs/applications/version-management/gitea/default.nix
+++ pkgs/applications/version-management/gitea/default.nix
@@ -21,12 +21,12 @@
 
 buildGoModule rec {
   pname = "gitea";
-  version = "1.21.0";
+  version = "1.21.1";
 
   # not fetching directly from the git repo, because that lacks several vendor files for the web UI
   src = fetchurl {
     url = "https://dl.gitea.com/gitea/${version}/gitea-src-${version}.tar.gz";
-    hash = "sha256-abEneLO18krs/wjY5RIuTt94S9ouQzW3fyu9BAShGpM=";
+    hash = "sha256-5WEHUMQsQNgrglS+xJ4IWHUl0a6RLLPyx0l+ECJ4R9g=";
   };
 
   vendorHash = null;
