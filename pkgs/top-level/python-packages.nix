6d578297deff14139114b4bb60ce841b49cd5842
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-12-20 04:45:17 +0100

    python3Packages.govee-api-laggat: init at 0.2.0
    TODO: upstream
    NOTE: required for home-assistant-custom-components.govee

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5102,6 +5102,8 @@ self: super: with self; {
 
   gorilla = callPackage ../development/python-modules/gorilla { };
 
+  govee-api-laggat = callPackage ../development/python-modules/govee-api-laggat { };
+
   govee-ble = callPackage ../development/python-modules/govee-ble { };
 
   govee-led-wez = callPackage ../development/python-modules/govee-led-wez { };
