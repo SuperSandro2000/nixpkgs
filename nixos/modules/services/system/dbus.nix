259d4afb66f58faf1b4608e3ab827af5dc199df0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-01-04 15:48:04 +0100

    nixos/dbus: reduce rebuild noise of makeDBusConf
    TODO: upstream?

--- nixos/modules/services/system/dbus.nix
+++ nixos/modules/services/system/dbus.nix
@@ -135,7 +135,16 @@ in
 
       services.dbus.packages = [
         cfg.dbusPackage
-        config.system.path
+        (pkgs.runCommandLocal "system-path-dbus"
+          {
+            __contentAddressed = true;
+          }
+          ''
+            mkdir -p $out/etc $out/share
+            cp -r ${config.system.path}/etc/dbus-1 $out/etc
+            cp -r ${config.system.path}/share/dbus-1 $out/share
+          ''
+        )
       ];
 
       systemd.user.sockets.dbus.wantedBy = [
