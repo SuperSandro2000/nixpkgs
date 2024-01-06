56ae205b8e6b67c47b0d8bb5605421a3554d5c0e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-01-06 15:45:12 +0100

    lib/make-iso9660-image: make iso building with remote builders easier

--- nixos/lib/make-iso9660-image.nix
+++ nixos/lib/make-iso9660-image.nix
@@ -48,6 +48,7 @@ assert usbBootable -> isohybridMbrImage != "";
 stdenv.mkDerivation {
   name = isoName;
   __structuredAttrs = true;
+  preferLocalBuild = true; # avoud copying the big squashfs
 
   buildCommandPath = ./make-iso9660-image.sh;
   nativeBuildInputs = [ xorriso syslinux zstd libossp_uuid ];
