ffe93c744a4ed386450ba122eab0d40a38b7f88b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-01-17 00:56:28 +0100

    vaultwarden: 2024.1.0 -> 2024.1.1

--- pkgs/tools/security/vaultwarden/webvault.nix
+++ pkgs/tools/security/vaultwarden/webvault.nix
@@ -1,20 +1,34 @@
 { lib
+, applyPatches
 , buildNpmPackage
 , fetchFromGitHub
+, fetchpatch
 , git
 , nixosTests
 , python3
 }:
 
 let
-  version = "2024.1.0";
+  version = "2024.1.1";
 
-  bw_web_builds = fetchFromGitHub {
-    owner = "dani-garcia";
-    repo = "bw_web_builds";
-    rev = "v${version}";
-    hash = "sha256-pR5fgpLcxnqURouandGIHRIfc3sn3QcfpU6mF6AxpeA=";
+  bw_web_builds = applyPatches {
+    src = fetchFromGitHub {
+      owner = "dani-garcia";
+      repo = "bw_web_builds";
+      rev = "v${version}";
+      hash = "sha256-xtfpxcJLP0C4FdnO45gsaecOWJ/cKC++Abm7iatTH1Y=";
+    };
+
+    patches = [
+      #  Fix applying latest patch if patches is a symlink
+      # https://github.com/dani-garcia/bw_web_builds/pull/152
+      (fetchpatch {
+        url = "https://github.com/dani-garcia/bw_web_builds/commit/5367c2447d9e2ec1e8d8493dadd7eeb80bbd5bce.patch";
+        hash = "sha256-z50w8xbQ98ZxBQWHUxK9KGLOjmgFsIsJoVnlA3rdwkw=";
+      })
+    ];
   };
+
 in buildNpmPackage rec {
   pname = "vaultwarden-webvault";
   inherit version;
@@ -23,10 +37,10 @@ in buildNpmPackage rec {
     owner = "bitwarden";
     repo = "clients";
     rev = "web-v${lib.removeSuffix "b" version}";
-    hash = "sha256-lDDy1b1yfw3nZrwEEkpvh6xYucgn20XHsGACc45eb2w=";
+    hash = "sha256-695iCkFhPEyyI4ekbjsdWpxgPy+bX392/X30HyL4F4Y=";
   };
 
-  npmDepsHash = "sha256-RR8Ua41D9SXymiPuabOnIab3byu8DR63rOfdeTaQpy4=";
+  npmDepsHash = "sha256-IJ5JVz9hHu3NOzFJAyzfhsMfPQgYQGntDEDuBMI/iZc=";
 
   postPatch = ''
     ln -s ${bw_web_builds}/{patches,resources} ..
