109e2db830a665e3049fb38cb37a43793e8ad8db
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-02-17 19:18:09 +0100

    home-assistant-custom-lovelace-modules.auto-entities: init at 1.12.1

--- pkgs/servers/home-assistant/custom-lovelace-modules/default.nix
+++ pkgs/servers/home-assistant/custom-lovelace-modules/default.nix
@@ -4,6 +4,8 @@
 {
   android-tv-card = callPackage ./android-tv-card { };
 
+  auto-entities = callPackage ./auto-entities { };
+
   button-card = callPackage ./button-card { };
 
   card-mod = callPackage ./card-mod { };
