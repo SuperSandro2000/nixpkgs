a87b02225dad0f3148a2ed0463c0cd0620afdc67
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-03-20 15:39:12 +0100

    nixos/top-level: make system ca

--- nixos/modules/system/activation/top-level.nix
+++ nixos/modules/system/activation/top-level.nix
@@ -62,6 +62,7 @@ let
   baseSystem = pkgs.stdenvNoCC.mkDerivation (
     {
       name = "nixos-system-${config.system.name}-${config.system.nixos.label}";
+      __contentAddressed = true;
       preferLocalBuild = true;
       allowSubstitutes = false;
       passAsFile = [ "extraDependencies" ];
