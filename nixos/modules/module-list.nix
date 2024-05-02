eafe28f6ba0267e833758077f7946c0c34755f59
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-02 11:03:03 +0200

    nixos/xkb.extraLayout: merge into xkb module
    services.xserver.exportConfiguration can be omitted, as sx and startx
    set this themselves already and that is the correct location for that
    setting anyway.
    
    environment.sessionVariables.XKB_CONFIG_ROOT is now always set to make
    things more consistent.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1929,7 +1929,6 @@
   ./services/x11/display-managers/startx.nix
   ./services/x11/display-managers/sx.nix
   ./services/x11/display-managers/xpra.nix
-  ./services/x11/extra-layouts.nix
   ./services/x11/fractalart.nix
   ./services/x11/hardware/digimend.nix
   ./services/x11/hardware/synaptics.nix
