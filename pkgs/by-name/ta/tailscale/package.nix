ada17a724d987a93a402201b3495edfa4888943c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-12 23:21:25 +0200

    tailscale: minify derivation

--- pkgs/by-name/ta/tailscale/package.nix
+++ pkgs/by-name/ta/tailscale/package.nix
@@ -58,12 +58,10 @@ buildGoModule {
     "cmd/derper"
     "cmd/derpprobe"
     "cmd/tailscaled"
-    "cmd/tsidp"
-    "cmd/get-authkey"
   ];
 
   excludedPackages = [
-    # exlude integration tests which fail to work
+    # exclude integration tests which fail to work
     # and require additional tooling
     "tstest/integration"
   ];
@@ -77,6 +75,11 @@ buildGoModule {
 
   tags = [
     "ts_include_cli"
+
+    # TODO https://github.com/NixOS/nixpkgs/pull/387742
+    # "ts_omit_aws"
+    # "ts_omit_bird"
+    # "ts_omit_kube"
   ];
 
   # remove vendored tooling to ensure it's not used
