a4c24b6df333d5fec88e6dd36c9f6d6dc56c75b0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-25 23:46:37 +0200

    spectacle: use minimal opencv

--- pkgs/kde/gear/spectacle/default.nix
+++ pkgs/kde/gear/spectacle/default.nix
@@ -7,6 +7,13 @@
 mkKdeDerivation {
   pname = "spectacle";
 
-  extraBuildInputs = [qtwayland qtmultimedia opencv];
+  extraBuildInputs = [
+    qtwayland
+    qtmultimedia
+    # https://invent.kde.org/graphics/spectacle/-/blob/master/CMakeLists.txt?ref_type=heads#L83
+    ((opencv.override { runAccuracyTests = false; }).overrideAttrs ({ cmakeFlags, ... }: {
+      cmakeFlags = cmakeFlags ++ [ "-DBUILD_LIST=core,imgproc" ];
+    }))
+  ];
   meta.mainProgram = "spectacle";
 }
