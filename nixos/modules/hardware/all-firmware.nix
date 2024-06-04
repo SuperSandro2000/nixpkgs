c000ce1ff136f3f602e0b357c20cc7937fd52b42
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-06-04 16:25:18 +0200

    nixos/all-firmware: drop random firmware

--- nixos/modules/hardware/all-firmware.nix
+++ nixos/modules/hardware/all-firmware.nix
@@ -36,14 +36,6 @@ in {
     (lib.mkIf (cfg.enableAllFirmware || cfg.enableRedistributableFirmware) {
       hardware.firmware = with pkgs; [
         linux-firmware
-        intel2200BGFirmware
-        rtl8192su-firmware
-        rt5677-firmware
-        rtl8761b-firmware
-        zd1211fw
-        alsa-firmware
-        sof-firmware
-        libreelec-dvb-firmware
       ] ++ lib.optional pkgs.stdenv.hostPlatform.isAarch raspberrypiWirelessFirmware;
     })
     (lib.mkIf cfg.enableAllFirmware {
