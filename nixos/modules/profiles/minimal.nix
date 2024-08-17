eebc8313f816fe83f9283bac80805cfbf73b05df
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-08-17 23:00:30 +0200

    nixos/minimal: drop now by default disabled options

--- nixos/modules/profiles/minimal.nix
+++ nixos/modules/profiles/minimal.nix
@@ -32,11 +32,4 @@ in
     logrotate.enable = mkDefault false;
     udisks2.enable = mkDefault false;
   };
-
-  xdg = {
-    autostart.enable = mkDefault false;
-    icons.enable = mkDefault false;
-    mime.enable = mkDefault false;
-    sounds.enable = mkDefault false;
-  };
 }
