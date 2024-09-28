18f5a2d4dac6d643a2a8611e1a22583526b04e01
Wolfgang Walther <walther@technowledgy.de>
2024-09-28 21:36:28 +0200

    postgresql_17: init at 17.0
    Release Notes:
    https://www.postgresql.org/docs/release/17.0/

--- pkgs/servers/sql/postgresql/generic.nix
+++ pkgs/servers/sql/postgresql/generic.nix
@@ -7,7 +7,7 @@ let
       , pkg-config, libxml2, tzdata, libkrb5, substituteAll, darwin
       , linux-pam
 
-      , removeReferencesTo, writeShellApplication
+      , removeReferencesTo, writeShellApplication, bison, flex, perl, docbook_xml_dtd_45, docbook-xsl-nons, libxslt
 
       # This is important to obtain a version of `libpq` that does not depend on systemd.
       , systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemdLibs && !stdenv.hostPlatform.isStatic
@@ -117,7 +117,8 @@ let
       pkg-config
       removeReferencesTo
     ]
-      ++ lib.optionals jitSupport [ llvmPackages.llvm.dev nukeReferences ];
+      ++ lib.optionals jitSupport [ llvmPackages.llvm.dev nukeReferences ]
+      ++ lib.optionals (atLeast "17") [ bison flex perl docbook_xml_dtd_45 docbook-xsl-nons libxslt ];
 
     enableParallelBuilding = true;
 
@@ -153,7 +154,8 @@ let
       ++ lib.optionals stdenv'.hostPlatform.isLinux [ "--with-pam" ]
       # This could be removed once the upstream issue is resolved:
       # https://postgr.es/m/flat/427c7c25-e8e1-4fc5-a1fb-01ceff185e5b%40technowledgy.de
-      ++ lib.optionals (stdenv'.hostPlatform.isDarwin && atLeast "16") [ "LDFLAGS_EX_BE=-Wl,-export_dynamic" ];
+      ++ lib.optionals (stdenv'.hostPlatform.isDarwin && atLeast "16") [ "LDFLAGS_EX_BE=-Wl,-export_dynamic" ]
+      ++ lib.optionals (atLeast "17") [ "--without-perl" ];
 
     patches = [
       (if atLeast "16" then ./patches/relative-to-symlinks-16+.patch else ./patches/relative-to-symlinks.patch)
