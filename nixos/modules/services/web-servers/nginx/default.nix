dae7bac47642a8be65b1037f83c3375e942a931e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-02 13:33:47 +0200

    treewide: fix all imports nginx/vhost-options.nix to treat it like a proper module

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -1384,7 +1384,7 @@ in
           '';
         }
       ]
-      ++ lib.flatten (map (host: host.assertions) (lib.attrValues virtualHosts))
+      ++ concatMap (host: host.assertions) (lib.attrValues virtualHosts)
       ++ map (
         name:
         mkCertOwnershipAssertion {
