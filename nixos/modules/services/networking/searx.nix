304a05b6fa9d558b5b90a3d8c88e36a445b48e5a
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-06 03:28:42 +0200

    nixos/searx: restart uwsgi when searx changes

--- nixos/modules/services/networking/searx.nix
+++ nixos/modules/services/networking/searx.nix
@@ -330,6 +330,11 @@ in
       uwsgi = mkIf cfg.configureUwsgi {
         requires = [ "searx-init.service" ];
         after = [ "searx-init.service" ];
+        restartTriggers = [
+          cfg.environmentFile
+          cfg.package
+          cfg.settingsFile
+        ];
       };
     };
 
