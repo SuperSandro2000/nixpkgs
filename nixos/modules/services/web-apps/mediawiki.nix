64c14e235eb05a56538a4db145c85e64b1b1a070
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-01-07 16:59:11 +0100

    nixos/mediawiki: fix gixy error

--- nixos/modules/services/web-apps/mediawiki.nix
+++ nixos/modules/services/web-apps/mediawiki.nix
@@ -602,6 +602,10 @@ in
              return 404;
           '';
         };
+        extraConfig = ''
+          open_file_cache max=1000;
+          open_file_cache_errors on;
+        '';
       };
     };
 
