f4c34c8c8bb399afea087817bfcbf0fa3ffaba7d
Shawn8901 <shawn8901@googlemail.com>
2025-01-11 23:29:22 +0100

    kdePackages.akonadi: enable support for SQLite and PostgreSQL backends

--- pkgs/kde/gear/akonadi/default.nix
+++ pkgs/kde/gear/akonadi/default.nix
@@ -6,8 +6,26 @@
   kaccounts-integration,
   shared-mime-info,
   xz,
+  withMariaDB ? true,
   mariadb,
+  withPostgreSQL ? false,
+  postgresql,
+  withSQLite ? false,
+  sqlite,
+  defaultBackend ? "MYSQL",
 }:
+
+assert (withMariaDB || withPostgreSQL || withSQLite);
+assert lib.assertOneOf "defaultBackend" defaultBackend [
+  "MYSQL"
+  "POSTGRES"
+  "SQLITE"
+];
+
+assert defaultBackend == "MYSQL" -> withMariaDB;
+assert defaultBackend == "POSTGRES" -> withPostgreSQL;
+assert defaultBackend == "SQLITE" -> withSQLite;
+
 mkKdeDerivation {
   pname = "akonadi";
 
@@ -16,24 +34,35 @@ mkKdeDerivation {
     ./ignore-mysql-config-timestamp.patch
   ];
 
-  extraCmakeFlags = [
-    "-DMYSQLD_SCRIPTS_PATH=${lib.getBin mariadb}/bin"
-  ];
+  extraCmakeFlags =
+    [ "-DDATABASE_BACKEND=${defaultBackend}" ]
+    ++ lib.optionals withMariaDB [
+      "-DMYSQLD_SCRIPTS_PATH=${lib.getBin mariadb}/bin"
+    ]
+    ++ lib.optionals withPostgreSQL [
+      "-DPOSTGRES_PATH=${lib.getBin postgresql}/bin"
+    ];
 
   extraNativeBuildInputs = [
     qttools
     shared-mime-info
   ];
-  extraBuildInputs = [
-    kaccounts-integration
-    accounts-qt
-    xz
-    mariadb
-  ];
+
+  extraBuildInputs =
+    [
+      kaccounts-integration
+      accounts-qt
+      xz
+    ]
+    ++ lib.optionals withMariaDB [ mariadb ]
+    ++ lib.optionals withPostgreSQL [ postgresql ]
+    ++ lib.optionals withSQLite [ sqlite ];
 
   # Hardcoded as a QString, which is UTF-16 so Nix can't pick it up automatically
   postFixup = ''
     mkdir -p $out/nix-support
-    echo "${mariadb}" > $out/nix-support/depends
+     ${lib.optionalString withMariaDB "echo '${mariadb}' >> $out/nix-support/depends"}
+     ${lib.optionalString withPostgreSQL "echo '${postgresql}' >> $out/nix-support/depends"}
+     ${lib.optionalString withSQLite "echo '${sqlite}' >> $out/nix-support/depends"}
   '';
 }
