6f99031540d769b0a7a4b9079c92df5e0b029326
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-02-16 15:22:43 +0100

    gixy: 0.2.1 -> 0.2.7

--- pkgs/by-name/gi/gixy/package.nix
+++ pkgs/by-name/gi/gixy/package.nix
@@ -24,14 +24,14 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "gixy";
-  version = "0.2.1";
+  version = "0.2.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dvershinin";
     repo = "gixy";
-    rev = "v${version}";
-    hash = "sha256-mOqCUIWqWDZdiGu+qQgPSSzs87q1DznZX62zDPKKakE=";
+    tag = "v${version}";
+    hash = "sha256-qGOvdmH4ZTk1v1ItHY9HaAgZtodmNgbUvJDa4JHfNzY=";
   };
 
   patches = [
