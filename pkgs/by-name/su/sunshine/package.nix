28d36ca155ee0028efcccf2cbdc64cf3477868ed
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-02-27 02:35:13 +0100

    sunshine: make config writeable by default

--- pkgs/by-name/su/sunshine/package.nix
+++ pkgs/by-name/su/sunshine/package.nix
@@ -67,6 +67,11 @@ stdenv'.mkDerivation rec {
     fetchSubmodules = true;
   };
 
+  patches = [
+    # otherwise the config is not writable after the initial copy and you cannot modify any Apps
+    ./make-apps-json-writable.diff
+  ];
+
   # build webui
   ui = buildNpmPackage {
     inherit src version;
@@ -230,7 +235,7 @@ stdenv'.mkDerivation rec {
   '';
 
   postInstall = ''
-    install -Dm644 ../packaging/linux/${pname}.desktop $out/share/applications/${pname}.desktop
+    install -Dm644 ../packaging/linux/sunshine.desktop $out/share/applications/sunshine.desktop
   '';
 
   doInstallCheck = true;
