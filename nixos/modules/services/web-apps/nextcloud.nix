27deadbe58200dd6e82c9b762bb7d6444644d9d8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-04-15 01:04:25 +0200

    systemd-lib: optimize for hydrogen, build things local instead of copying all things remote first and then back

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -120,7 +120,7 @@ let
 
   requiresRuntimeSystemdCredentials = (lib.length runtimeSystemdCredentials) != 0;
 
-  occ = pkgs.writeShellApplication {
+  occ = (pkgs.writeShellApplication {
     name = "nextcloud-occ";
 
     text =
@@ -177,7 +177,9 @@ let
           exec ${command}
         fi
       '';
-  };
+  }).overrideAttrs (_: {
+    preferLocalBuild = true;
+  });
 
   inherit (config.system) stateVersion;
 
