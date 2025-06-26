bd486659f201e2ec38cdf48acccb28b6d2fe24ec
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-06-26 18:37:48 +0200

    nixos/gitea: don't use optionalAttrs
    We don't want to do things around the module system here
    
    (cherry picked from commit 70e2f91f1093547c11e1cf7c1ba5e3ed455ac4d7)

--- nixos/modules/services/misc/gitea.nix
+++ nixos/modules/services/misc/gitea.nix
@@ -790,7 +790,7 @@ in
         ];
       };
 
-    services.postgresql = lib.optionalAttrs (usePostgresql && cfg.database.createDatabase) {
+    services.postgresql = lib.mkIf (usePostgresql && cfg.database.createDatabase) {
       enable = lib.mkDefault true;
 
       ensureDatabases = [ cfg.database.name ];
@@ -802,7 +802,7 @@ in
       ];
     };
 
-    services.mysql = lib.optionalAttrs (useMysql && cfg.database.createDatabase) {
+    services.mysql = lib.mkIf (useMysql && cfg.database.createDatabase) {
       enable = lib.mkDefault true;
       package = lib.mkDefault pkgs.mariadb;
 
