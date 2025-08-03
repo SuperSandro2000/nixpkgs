4bc9f2ac8fccde0a369a991c6938c3823676eeb4
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-04 01:08:41 +0200

    tailscale: minify

--- pkgs/by-name/ta/tailscale/package.nix
+++ pkgs/by-name/ta/tailscale/package.nix
@@ -58,8 +58,6 @@ buildGoModule {
     "cmd/derper"
     "cmd/derpprobe"
     "cmd/tailscaled"
-    "cmd/tsidp"
-    "cmd/get-authkey"
   ];
 
   excludedPackages = [
@@ -76,6 +74,11 @@ buildGoModule {
 
   tags = [
     "ts_include_cli"
+
+    # TODO https://github.com/NixOS/nixpkgs/pull/387742
+    # "ts_omit_aws"
+    # "ts_omit_bird"
+    # "ts_omit_kube"
   ];
 
   # Remove vendored tooling to ensure it's not used; also avoids some unnecessary tests
