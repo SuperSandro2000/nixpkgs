2495e0191f0235cf7d7612ab4aedfc238bc4a7bc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-09-05 20:30:02 +0200

    clean out

--- pkgs/by-name/op/opencl-clang/llvm/common/patches.nix
+++ pkgs/by-name/op/opencl-clang/llvm/common/patches.nix
@@ -19,56 +19,6 @@
       path = ../15;
     }
   ];
-  "libunwind/gnu-install-dirs.patch" = [
-    {
-      path = ../15;
-    }
-  ];
-  "compiler-rt/X86-support-extension.patch" = [
-    {
-      path = ../15;
-    }
-  ];
-  "compiler-rt/armv6-scudo-libatomic.patch" = [
-    {
-      path = ../15;
-    }
-  ];
-  "compiler-rt/armv7l.patch" = [
-    {
-      path = ../13;
-    }
-  ];
-  "compiler-rt/gnu-install-dirs.patch" = [
-    {
-      path = ../15;
-    }
-  ];
-  "compiler-rt/darwin-targetconditionals.patch" = [
-    {
-      path = ../13;
-    }
-  ];
-  "compiler-rt/codesign.patch" = [
-    {
-      path = ../13;
-    }
-  ];
-  "compiler-rt/normalize-var.patch" = [
-    {
-      path = ../12;
-    }
-  ];
-  "lldb/procfs.patch" = [
-    {
-      path = ../15;
-    }
-  ];
-  "lldb/resource-dir.patch" = [
-    {
-      path = ../12;
-    }
-  ];
   "llvm/no-pipes.patch" = [
     {
       path = ../12;
