a8b199b26ae4d69aa138bfdf47c6892317d4a19c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-05-22 01:38:09 +0200

    python314Packages.aiovban-pyaudio: init at 1.1.0

---
+++ pkgs/development/python-modules/aiovban-pyaudio/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  aiovban,
+  buildPythonPackage,
+  pyaudio,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "aiovban-pyaudio";
+  inherit (aiovban) version pyproject src;
+
+  sourceRoot = "${aiovban.src.name}/aiovban_pyaudio";
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiovban
+    pyaudio
+  ];
+
+  pythonImportsCheck = [
+    "aiovban_pyaudio"
+  ];
+
+  meta = {
+    changelog = "https://github.com/wmbest2/aiovban/releases/tag/${finalAttrs.src.tag}";
+    description = "PyAudio wrapper for aiovban";
+    homepage = "https://github.com/wmbest2/aiovban/tree/main/aiovban_pyaudio";
+    license = lib.licenses.mit;
+    inherit (aiovban.meta) maintainers;
+  };
+})
