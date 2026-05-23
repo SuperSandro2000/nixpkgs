3820f27c94ec15137222f06dbc90938b6cb96c32
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-05-24 00:25:19 +0200

    gixy: 0.2.1 -> 0.3.4

--- pkgs/by-name/gi/gixy/package.nix
+++ pkgs/by-name/gi/gixy/package.nix
@@ -1,53 +1,33 @@
 {
   lib,
   fetchFromGitHub,
-  python3,
+  python3Packages,
   nginx,
 }:
 
-let
-  python = python3.override {
-    self = python;
-    packageOverrides = self: super: {
-      pyparsing = super.pyparsing.overridePythonAttrs rec {
-        version = "2.4.7";
-        src = fetchFromGitHub {
-          owner = "pyparsing";
-          repo = "pyparsing";
-          rev = "pyparsing_${version}";
-          sha256 = "14pfy80q2flgzjcx8jkracvnxxnr59kjzp3kdm5nh232gk1v6g6h";
-        };
-        nativeBuildInputs = [ super.setuptools ];
-      };
-    };
-  };
-in
-python.pkgs.buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   pname = "gixy";
-  version = "0.2.1";
+  version = "0.3.4";
   pyproject = true;
 
   src = fetchFromGitHub {
-    owner = "dvershinin";
-    repo = "gixy";
+    owner = "MegaManSec";
+    repo = "Gixy-Next";
     rev = "v${version}";
-    hash = "sha256-mOqCUIWqWDZdiGu+qQgPSSzs87q1DznZX62zDPKKakE=";
+    hash = "sha256-i4LBhSd5HSqzvvYLgm7JVO4pnMRhsGmDKI2FItp/1mE=";
   };
 
-  patches = [
-    ./python3.13-compat.patch
-  ];
+  build-system = [ python3Packages.setuptools ];
 
-  build-system = [ python.pkgs.setuptools ];
-
-  dependencies = with python.pkgs; [
+  dependencies = with python3Packages; [
     configargparse
-    pyparsing
+    crossplane
     jinja2
-    six
+    pyparsing
+    tldextract
   ];
 
-  nativeCheckInputs = with python.pkgs; [
+  nativeCheckInputs = with python3Packages; [
     pytestCheckHook
     pytest-xdist
   ];
@@ -63,8 +43,7 @@ python.pkgs.buildPythonApplication rec {
       Gixy is a tool to analyze Nginx configuration.
       The main goal of Gixy is to prevent security misconfiguration and automate flaw detection.
     '';
-    homepage = "https://github.com/yandex/gixy";
-    sourceProvenance = [ lib.sourceTypes.fromSource ];
+    homepage = "https://github.com/MegaManSec/Gixy-Next";
     license = lib.licenses.mpl20;
     maintainers = [ lib.maintainers.SuperSandro2000 ];
     platforms = lib.platforms.unix;
