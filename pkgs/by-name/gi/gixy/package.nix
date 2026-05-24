3215303c926faeb0b8389c80348ad13c72b876cc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-05-24 02:43:29 +0200

    Revert "gixy: 0.2.1 -> 0.3.4"
    This reverts commit 9a0dc518e9767cfd4c1a4e34f3b51a8529c8931a.

--- pkgs/by-name/gi/gixy/package.nix
+++ pkgs/by-name/gi/gixy/package.nix
@@ -1,33 +1,53 @@
 {
   lib,
   fetchFromGitHub,
-  python3Packages,
+  python3,
   nginx,
 }:
 
-python3Packages.buildPythonApplication rec {
+let
+  python = python3.override {
+    self = python;
+    packageOverrides = self: super: {
+      pyparsing = super.pyparsing.overridePythonAttrs rec {
+        version = "2.4.7";
+        src = fetchFromGitHub {
+          owner = "pyparsing";
+          repo = "pyparsing";
+          rev = "pyparsing_${version}";
+          sha256 = "14pfy80q2flgzjcx8jkracvnxxnr59kjzp3kdm5nh232gk1v6g6h";
+        };
+        nativeBuildInputs = [ super.setuptools ];
+      };
+    };
+  };
+in
+python.pkgs.buildPythonApplication rec {
   pname = "gixy";
-  version = "0.3.4";
+  version = "0.2.1";
   pyproject = true;
 
   src = fetchFromGitHub {
-    owner = "MegaManSec";
-    repo = "Gixy-Next";
+    owner = "dvershinin";
+    repo = "gixy";
     rev = "v${version}";
-    hash = "sha256-i4LBhSd5HSqzvvYLgm7JVO4pnMRhsGmDKI2FItp/1mE=";
+    hash = "sha256-mOqCUIWqWDZdiGu+qQgPSSzs87q1DznZX62zDPKKakE=";
   };
 
-  build-system = [ python3Packages.setuptools ];
-
-  dependencies = with python3Packages; [
-    configargparse
-    crossplane
-    jinja2
-    pyparsing
-    tldextract
+  patches = [
+    ./python3.13-compat.patch
   ];
 
-  nativeCheckInputs = with python3Packages; [
+  build-system = [ python.pkgs.setuptools ];
+
+  dependencies = with python.pkgs; [
+    configargparse
+    pyparsing
+    jinja2
+    six
+  ];
+
+  nativeCheckInputs = with python.pkgs; [
     pytestCheckHook
     pytest-xdist
   ];
@@ -43,7 +63,8 @@ python3Packages.buildPythonApplication rec {
       Gixy is a tool to analyze Nginx configuration.
       The main goal of Gixy is to prevent security misconfiguration and automate flaw detection.
     '';
-    homepage = "https://github.com/MegaManSec/Gixy-Next";
+    homepage = "https://github.com/yandex/gixy";
+    sourceProvenance = [ lib.sourceTypes.fromSource ];
     license = lib.licenses.mpl20;
     maintainers = [ lib.maintainers.SuperSandro2000 ];
     platforms = lib.platforms.unix;
