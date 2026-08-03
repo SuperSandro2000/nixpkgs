2571105e58740a5f9cec0fddaaafdcc1c6fce020
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-04 01:22:11 +0200

    music-assistant: 2.9.13 -> 2.10.0
    Changelog: https://github.com/music-assistant/server/releases/tag/2.10.0
    Diff: https://github.com/music-assistant/server/compare/2.9.13...2.10.0

--- nixos/modules/services/audio/music-assistant.nix
+++ nixos/modules/services/audio/music-assistant.nix
@@ -140,8 +140,7 @@ in
           lsof
         ]
         ++ lib.optionals (lib.elem "airplay" cfg.providers) [
-          cliairplay
-          libraop
+          airplay-cli
         ]
         ++ lib.optionals (lib.elem "airplay_receiver" cfg.providers) [
           shairport-sync
