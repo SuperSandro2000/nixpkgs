dfb2bf8b4c1de1173bef0afc1ee06dbeedde32cf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-21 04:27:40 +0200

    python3Packages.pyloudnorm: init at 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15390,6 +15390,8 @@ self: super: with self; {
 
   pylookyloo = callPackage ../development/python-modules/pylookyloo { };
 
+  pyloudnorm = callPackage ../development/python-modules/pyloudnorm { };
+
   pylpsd = callPackage ../development/python-modules/pylpsd { };
 
   pylru = callPackage ../development/python-modules/pylru { };
