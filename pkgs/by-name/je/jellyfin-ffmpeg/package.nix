a8e84df487d3ca2cf4ad7b1e62487a43fb1d3a62
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-09-10 01:33:02 +0200

    jellyfin-ffmpeg: disable more big and unused dependencies

--- pkgs/by-name/je/jellyfin-ffmpeg/package.nix
+++ pkgs/by-name/je/jellyfin-ffmpeg/package.nix
@@ -18,8 +18,15 @@ in
   };
   buildFfplay = false; # requires SDL2 which gets disabled
   buildFfprobe = true; # required by various programs like Immich
-  withSamba = false; # samba is rather big and unused
+
+  # dependencies with big closure sizes that are not enabled by upstream in
+  # https://github.com/jellyfin/jellyfin-ffmpeg/tree/jellyfin/builder/scripts.d
+  withFlite = false;
+  withFrei0r = false;
+  withRav1e = false;
+  withSamba = false;
   withSdl2 = false;
+  withWhisper = false;
 }).overrideAttrs
   (old: {
     pname = "jellyfin-ffmpeg";
